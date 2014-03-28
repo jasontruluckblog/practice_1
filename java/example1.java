9a528073029a74126c7c0ea8c0912eb05bdc1f7d
Jason Truluck <jason.truluck@gmail.com>
2014-03-28 17:06:15 -0400

    Update example1.java

--- java/example1.java
+++ java/example1.java
@@ -2,8 +2,6 @@ import java.util.ArrayList;
 /**
  * Write a program to determine all pairs of positive integers (a,b)
  * such that a < b < 1000 and (a^2+b^2+1)/(ab) is an Integer.
- *
- * Jason Truluck 3/25/14.
  */
 public class example1 {
     public static void main(String[] args) {
